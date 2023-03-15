c7a9eac58b16dae16907e901ddf153adcbbff480
JanardhanReddy <bommireddy246@gmail.com>
2023-03-15 02:19:27 +0000

    pipeline update shared library

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "https://github.com/janardhanReddy-B/tf-module-vpc.git"
+  source = "github.com/janardhanReddy-B/tf-module-vpc.git"
   ENV = var.ENV
   PROJECT = var.PROJECT
   VPC_CIDR = var.VPC_CIDR
